c2eeada9d27f1340418ab200cbf10b697d2a22fb
Ashley Huxley <git@electricfox.co.uk>
2024-09-08 11:50:44 +0100

    ci: publish to docker

--- SondeAlert/Program.cs
+++ SondeAlert/Program.cs
@@ -2,14 +2,31 @@ using ElectricFox.SondeAlert;
 using ElectricFox.SondeAlert.Mqtt;
 using ElectricFox.SondeAlert.Options;
 using ElectricFox.SondeAlert.Telegram;
+using NLog;
+using NLog.Extensions.Logging;
+using NLog.Web;
+
+var configBuilder = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables();
+
+LogManager.Setup().LoadConfigurationFromAppSettings();
+
+var logger = LogManager.GetCurrentClassLogger();
+logger.Debug("Initialising SondeAlert");
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddLogging(
+    c => {
+        c.ClearProviders();
+        c.AddNLog();
+    }
+);
+
 builder.Services.AddHostedService<Worker>();
 
-IConfigurationRoot configRoot = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json")
-    .AddEnvironmentVariables()
-    .Build();
+IConfigurationRoot configRoot = configBuilder.Build();
 
 builder.Services.Configure<SondeAlertOptions>(configRoot.GetSection("SondeAlert"));
 builder.Services.Configure<TelegramOptions>(configRoot.GetSection("Telegram"));
